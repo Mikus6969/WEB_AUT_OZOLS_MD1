4fa2aa7bee543efb918e1de38eb44e159d8c6ee8
Mikus Ozols <mikus.ozols2004@gmail.com>
2025-05-27 17:27:49 +0300

    WORKS

--- cypress/e2e/selectableGrid.cy.js
+++ cypress/e2e/selectableGrid.cy.js
@@ -4,20 +4,20 @@ const selectablePage = new SelectablePage();
 
 describe('Selectable Grid Tests', () => {
   it('should highlight the correct grid items and leave others unselected', () => {
-    // Step a: Visit the page
+   
     selectablePage.visit();
 
-    // Step b: Click "Grid"
+ 
     selectablePage.clickGridTab();
 
-    // Step c: Click “Two”, “Four”, “Six”, “Eight”
+    // Click Two, Four, Six, Eight
     const selectedItems = ['Two', 'Four', 'Six', 'Eight'];
     selectedItems.forEach(item => selectablePage.clickGridItem(item));
 
-    // Step d: Validate selected items are highlighted
+    // Validate selected items are highlighted
     selectedItems.forEach(item => selectablePage.isItemSelected(item));
 
-    // Step e: Validate unselected items are not highlighted
+    // Validate unselected items are not highlighted
     const unselectedItems = ['One', 'Three', 'Five', 'Seven', 'Nine'];
     unselectedItems.forEach(item => selectablePage.isItemNotSelected(item));
   });
